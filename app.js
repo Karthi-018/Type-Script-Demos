d921c79913454db7c20fd6f4dfcb14a6f36fc86d
Karthi <karthi.learning.2020@gmail.com>
2022-11-09 12:30:12 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    var res = n1 + n2;
+    return res;
+}
+console.log('The Sum of two Number si ' + add(10, 20));
+var n = 10;
+console.log(n * n);
---
+++ demo.ts
@@ -0,0 +1,8 @@
+function add(n1,n2)
+{
+    var res = n1+n2;
+
+    return res;
+}
+
+console.log('The Sum of two Number si '+ add(10,20))
